3a866eef3ae7fe5e635ea9ad7d9700ade270f184
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-17 21:06:34 -0300

    feat: ensure UploadLocalFile calls UUIDGenerator with correct input

--- src/domain/contracts/gateways/index.ts
+++ src/domain/contracts/gateways/index.ts
@@ -1,1 +1,2 @@
 export * from './file-storage'
+export * from './uuid'
--- src/domain/usecases/upload-local-file.ts
+++ src/domain/usecases/upload-local-file.ts
@@ -1,12 +1,13 @@
-import { UploadFile } from '@/domain/contracts/gateways'
+import { UploadFile, UUIDGenerator } from '@/domain/contracts/gateways'
 import { UploadError } from '../entities/errors'
 
-type Setup = (fileStorage: UploadFile) => UploadLocalFile
+type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => UploadLocalFile
 type Input = { file: { buffer: Buffer, mimeType: string, fileName: string } }
 type Output = { fileName: string }
 export type UploadLocalFile = (input: Input) => Promise<Output>
 
-export const setupUploadLocalFile: Setup = (fileStorage) => async ({ file }) => {
+export const setupUploadLocalFile: Setup = (fileStorage, crypto) => async ({ file }) => {
+  crypto.uuid({ key: file.fileName })
   const uploadResponse = await fileStorage.upload({ file: file.buffer, fileName: `${file.fileName}.${file.mimeType.split('/')[1]}` })
   if (uploadResponse instanceof Error) throw new UploadError(file.fileName)
   return { fileName: file.fileName }

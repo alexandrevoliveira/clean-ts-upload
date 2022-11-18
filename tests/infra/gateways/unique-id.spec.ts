b71ac9c5b6ad021a2520130fe6c20930caf9ea27
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-17 22:11:17 -0300

    test: ensure UniqueId returns correct uuid

---
+++ tests/infra/gateways/unique-id.spec.ts
@@ -0,0 +1,22 @@
+import { UniqueId } from '@/infra/gateways'
+
+import { reset, set } from 'mockdate'
+
+describe('UniqueId', () => {
+  let sut: UniqueId
+
+  beforeAll(() => {
+    set(new Date(2022, 1, 15, 12, 0, 15))
+    sut = new UniqueId()
+  })
+
+  afterAll(() => {
+    reset()
+  })
+
+  it('should create a unique id', () => {
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_20220215120015')
+  })
+})

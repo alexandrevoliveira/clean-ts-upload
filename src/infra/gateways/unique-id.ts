4d3c4096e1a5a62c08e97f3ae2590042ec7dc513
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-17 22:10:59 -0300

    feat: ensure UniqueId returns correct uuid

---
+++ src/infra/gateways/unique-id.ts
@@ -0,0 +1,15 @@
+import { UUIDGenerator } from '@/domain/contracts/gateways'
+
+export class UniqueId implements UUIDGenerator {
+  uuid ({ key }: UUIDGenerator.Input): string {
+    const date = new Date()
+    return key +
+      '_' +
+      date.getFullYear().toString() +
+      `${date.getMonth() + 1}`.padStart(2, '0') +
+      date.getDate().toString().padStart(2, '0') +
+      date.getHours().toString().padStart(2, '0') +
+      date.getMinutes().toString().padStart(2, '0') +
+      date.getSeconds().toString().padStart(2, '0')
+  }
+}

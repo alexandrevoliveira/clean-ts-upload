eb92868ee7dbaf2144d4d8223b5ffa0bf251c364
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-19 17:05:38 -0300

    feat: ensure ValidationBuilder returns Required and RequiredBuffer

--- src/application/validation/builder.ts
+++ src/application/validation/builder.ts
@@ -16,6 +16,9 @@ export class ValidationBuilder {
       this.validators.push(new RequiredBuffer(this.value))
     } else {
       this.validators.push(new Required(this.value, this.fieldName))
+      if (this.value.buffer !== undefined) {
+        this.validators.push(new RequiredBuffer(this.value.buffer))
+      }
     }
     return this
   }

891670d2319d8eea81dd6412492377f897cb351f
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-19 16:59:53 -0300

    test: ensure ValidationBuilder returns RequiredBuffer

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,9 +1,17 @@
-import { Required, ValidationBuilder } from '@/application/validation'
+import { Required, RequiredBuffer, ValidationBuilder } from '@/application/validation'
 
 describe('ValidationBuilder', () => {
   it('should return Required', () => {
     const validators = ValidationBuilder.of({ value: 'any_value' }).required().build()
 
     expect(validators).toEqual([new Required('any_value')])
+  })
+
+  it('should return RequiredBuffer', () => {
+    const buffer = Buffer.from('any_buffer')
+
+    const validators = ValidationBuilder.of({ value: buffer }).required().build()
+
+    expect(validators).toEqual([new RequiredBuffer(buffer)])
   })
 })

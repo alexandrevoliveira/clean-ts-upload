c08c89f428b9b5c92521c53ac60719569160e125
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-19 00:27:05 -0300

    feat: ensure RequiredBuffer validator extends Required

--- src/application/validation/required.ts
+++ src/application/validation/required.ts
@@ -3,11 +3,24 @@ import { Validator } from '@/application/validation'
 
 export class Required implements Validator {
   constructor (
-    readonly value: string,
+    readonly value: any,
     readonly fieldName?: string
   ) {}
 
   validate (): Error | undefined {
     if (this.value === null || this.value === undefined) return new RequiredFieldError(this.fieldName)
+  }
+}
+
+export class RequiredBuffer extends Required {
+  constructor (
+    override readonly value: Buffer,
+    override readonly fieldName?: string
+  ) {
+    super(value, fieldName)
+  }
+
+  override validate (): Error | undefined {
+    if (super.validate() !== undefined) return new RequiredFieldError(this.fieldName)
   }
 }

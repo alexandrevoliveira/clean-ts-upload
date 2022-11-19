d813c5d4b537b5147c9eb381a37a88755afb4744
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-19 00:10:04 -0300

    test: ensure Required validator returns RequiredFieldError if value is undefined

--- tests/application/validation/required.spec.ts
+++ tests/application/validation/required.spec.ts
@@ -9,4 +9,12 @@ describe('Required', () => {
 
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
+
+  it('should return RequiredFieldError if value is undefined', () => {
+    const sut = new Required(undefined as any, 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
 })

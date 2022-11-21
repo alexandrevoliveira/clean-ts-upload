f5f10f634dec6e3ff388ad9b7e0ecd2fa873ec39
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-21 15:22:44 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
-import { bodyParser, contentType } from '@/main/middlewares'
+import { bodyParser, contentType, cors } from '@/main/middlewares'
 
 import { Express } from 'express'
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser)
   app.use(contentType)
+  app.use(cors)
 }

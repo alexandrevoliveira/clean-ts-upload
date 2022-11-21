79590b5d83a7c5b19415350a47abd3e1b8a9be3c
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-21 19:54:50 -0300

    feat: ensure adaptExpressRoute returns 200 and correct data

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -2,5 +2,6 @@ import { Controller } from '@/application/controllers'
 import { Request, Response } from 'express'
 
 export const adaptExpressRoute = (controller: Controller) => async (req: Request, res: Response): Promise<void> => {
-  await controller.handle({ ...req.body, ...req.locals })
+  const httpResponse = await controller.handle({ ...req.body, ...req.locals })
+  res.status(200).json(httpResponse.data)
 }

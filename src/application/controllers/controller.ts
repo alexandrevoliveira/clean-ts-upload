8c821104c988c58c263afe4e175dcb1930944cfc
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-20 23:05:40 -0300

    feat: ensure Controller returns 400 if validation fails

---
+++ src/application/controllers/controller.ts
@@ -0,0 +1,23 @@
+import { HttpResponse } from '@/application/helpers'
+import { ValidationComposite, Validator } from '@/application/validation'
+
+export abstract class Controller {
+  abstract perform (httpRequest: any): Promise<HttpResponse>
+
+  buildValidators (httpRequest: any): Validator[] {
+    return []
+  }
+
+  async handle (httpRequest: any): Promise<HttpResponse> {
+    const error = this.validate(httpRequest)
+    return {
+      statusCode: 400,
+      data: error
+    }
+  }
+
+  private validate (httpRequest: any): Error | undefined {
+    const validators = this.buildValidators(httpRequest)
+    return new ValidationComposite(validators).validate()
+  }
+}

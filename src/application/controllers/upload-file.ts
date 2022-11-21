037742ebb2fb792cc2e94fd908313b731bafd7a8
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-21 12:04:39 -0300

    feat: ensure UploadFileController returns 200 if UploadLocalFile succeeds

--- src/application/controllers/upload-file.ts
+++ src/application/controllers/upload-file.ts
@@ -1,19 +1,17 @@
 import { UploadLocalFile } from '@/domain/usecases'
 import { Controller } from '@/application/controllers'
-import { HttpResponse } from '../helpers'
+import { HttpResponse, ok } from '@/application/helpers'
 
 type HttpRequest = { file: { buffer: Buffer, mimeType: string, fileName: string } }
+type Model = { fileName: string }
 
 export class UploadFileController extends Controller {
   constructor (private readonly uploadLocalFile: UploadLocalFile) {
     super()
   }
 
-  async perform ({ file }: HttpRequest): Promise<HttpResponse<any>> {
-    await this.uploadLocalFile({ file })
-    return {
-      statusCode: 200,
-      data: 'any_data'
-    }
+  async perform ({ file }: HttpRequest): Promise<HttpResponse<Model>> {
+    const fileName = await this.uploadLocalFile({ file })
+    return ok(fileName)
   }
 }

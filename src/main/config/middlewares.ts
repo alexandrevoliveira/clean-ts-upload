48a043a579d79b949b5e1a097dccbaeebf2bd350
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-21 15:12:23 -0300

    feat: ensure requests returns json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,8 @@
-import { bodyParser } from '@/main/middlewares'
+import { bodyParser, contentType } from '@/main/middlewares'
 
 import { Express } from 'express'
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser)
+  app.use(contentType)
 }

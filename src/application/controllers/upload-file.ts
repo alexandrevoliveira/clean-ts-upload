150b7c9f6e92471bcd72714f401de04d6ee4bd8e
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-21 00:21:51 -0300

    feat: ensure UploadFileController extends Controller

--- src/application/controllers/upload-file.ts
+++ src/application/controllers/upload-file.ts
@@ -1,11 +1,19 @@
 import { UploadLocalFile } from '@/domain/usecases'
+import { Controller } from '@/application/controllers'
+import { HttpResponse } from '../helpers'
 
 type HttpRequest = { file: { buffer: Buffer, mimeType: string, fileName: string } }
 
-export class UploadFileController {
-  constructor (private readonly uploadLocalFile: UploadLocalFile) {}
+export class UploadFileController extends Controller {
+  constructor (private readonly uploadLocalFile: UploadLocalFile) {
+    super()
+  }
 
-  async handle ({ file }: HttpRequest): Promise<void> {
+  async perform ({ file }: HttpRequest): Promise<HttpResponse<any>> {
     await this.uploadLocalFile({ file })
+    return {
+      statusCode: 200,
+      data: 'any_data'
+    }
   }
 }

89a3adc8b5059e81f65bc0ee7ac4a07540e67989
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-21 19:44:49 -0300

    test: ensure adaptExpressRoute calls handle with correct request

---
+++ tests/main/adapters/express-router.spec.ts
@@ -0,0 +1,32 @@
+import { Controller } from '@/application/controllers'
+import { adaptExpressRoute } from '@/main/adapters'
+
+import { NextFunction, Request, RequestHandler, Response } from 'express'
+import { mock, MockProxy } from 'jest-mock-extended'
+import { getMockReq, getMockRes } from '@jest-mock/express'
+
+describe('adaptExpressRoute', () => {
+  let req: Request
+  let res: Response
+  let next: NextFunction
+  let controller: MockProxy<Controller>
+  let sut: RequestHandler
+
+  beforeAll(() => {
+    req = getMockReq({ body: { anyBody: 'any_body' }, locals: { anyLocals: 'any_locals' } })
+    res = getMockRes().res
+    next = getMockRes().next
+    controller = mock()
+  })
+
+  beforeEach(() => {
+    sut = adaptExpressRoute(controller)
+  })
+
+  it('should call handle with correct request', async () => {
+    await sut(req, res, next)
+
+    expect(controller.handle).toHaveBeenCalledTimes(1)
+    expect(controller.handle).toHaveBeenCalledWith({ anyBody: 'any_body', anyLocals: 'any_locals' })
+  })
+})

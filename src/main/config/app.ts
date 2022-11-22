4e4d541346dbf44f9b549b1757fadf7282454019
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-22 13:01:00 -0300

    feat: add routes config file

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import { setupMiddlewares } from '@/main/config/middlewares'
+import { setupRoutes } from '@/main/config/routes'
 
 import express from 'express'
 
 const app = express()
 setupMiddlewares(app)
+setupRoutes(app)
 export { app }

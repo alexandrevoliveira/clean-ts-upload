28cfea04532e235603662d279bc4cda2d532e627
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-21 23:40:22 -0300

    feat: ensure adaptMulter returns 500 if upload fails

--- src/main/adapters/multer.ts
+++ src/main/adapters/multer.ts
@@ -1,9 +1,13 @@
+import { ServerError } from '@/application/errors'
 import { RequestHandler } from 'express'
 import multer from 'multer'
 
 export const adaptMulter: RequestHandler = (req, res, next) => {
   const upload = multer().single('file')
   upload(req, res, err => {
-    if (err === undefined) next()
+    if (err !== undefined) {
+      return res.status(500).json({ error: new ServerError(err).message })
+    }
+    next()
   })
 }

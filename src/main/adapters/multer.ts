78f82f987199a3bb7a0c514c08f92292941c9f98
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-21 23:55:17 -0300

    feat: ensure adaptMulter add file to req.locals

--- src/main/adapters/multer.ts
+++ src/main/adapters/multer.ts
@@ -1,4 +1,5 @@
 import { ServerError } from '@/application/errors'
+
 import { RequestHandler } from 'express'
 import multer from 'multer'
 
@@ -7,6 +8,16 @@ export const adaptMulter: RequestHandler = (req, res, next) => {
   upload(req, res, err => {
     if (err !== undefined) {
       return res.status(500).json({ error: new ServerError(err).message })
+    }
+    if (req.file !== undefined) {
+      req.locals = {
+        ...req.locals,
+        file: {
+          buffer: req.file.buffer,
+          mimeType: req.file.mimetype,
+          fileName: req.file.originalname
+        }
+      }
     }
     next()
   })

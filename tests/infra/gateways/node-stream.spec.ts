605488e6334d748c181cb87910f456b220e4912d
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-09-10 23:13:38 -0300

    test: ensure MultiStream receives correct options

--- tests/infra/gateways/node-stream.spec.ts
+++ tests/infra/gateways/node-stream.spec.ts
@@ -1,32 +1,52 @@
-import { NodeStream } from '@/infra/gateways'
+import { MultiStream, NodeStream } from '@/infra/gateways'
 import { exec } from 'child_process'
 import { readdirSync } from 'fs'
 import { promisify } from 'util'
 
-describe('NodeStream', () => {
-  let itemPath: string
-  let file: Buffer
-  let sut: NodeStream
+describe('Streams', () => {
+  describe('NodeStream', () => {
+    let itemPath: string
+    let file: Buffer
+    let sut: NodeStream
 
-  beforeAll(async () => {
-    file = Buffer.from('any_file_content')
-    itemPath = 'tmp/any_file.csv'
+    beforeAll(async () => {
+      file = Buffer.from('any_file_content')
+      itemPath = 'tmp/any_file.csv'
+    })
+
+    beforeEach(() => {
+      sut = new NodeStream()
+    })
+
+    afterEach(async () => {
+      const execAsync = promisify(exec)
+      await execAsync('rm -rf tmp/any_file.csv')
+    })
+
+    it('should write on right path', async () => {
+      await sut.writeOn({ item: file, itemPath })
+
+      const dir = readdirSync('tmp')
+
+      expect(dir).toContain('any_file.csv')
+    })
   })
 
-  beforeEach(() => {
-    sut = new NodeStream()
-  })
+  describe('MultiStream', () => {
+    it('should have undefined options if file is instanceof Buffer or typeof string', async () => {
+      const file = Buffer.from('any_file_content')
 
-  afterEach(async () => {
-    const execAsync = promisify(exec)
-    await execAsync('rm -rf tmp/any_file.csv')
-  })
+      const sut = MultiStream.readStream({ item: file })
 
-  it('should write on right path', async () => {
-    await sut.writeOn({ item: file, itemPath })
+      expect(sut.multiStreamOptions).toBeUndefined()
+    })
 
-    const dir = readdirSync('tmp')
+    it('should have options with objectMode equal to true if file is neither instanceof Buffer nor typeof string', async () => {
+      const file = { anything: 'any_object' }
 
-    expect(dir).toContain('any_file.csv')
+      const sut = MultiStream.readStream({ item: file })
+
+      expect(sut.multiStreamOptions).toEqual({ objectMode: true })
+    })
   })
 })

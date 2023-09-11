5c2b75e6eae05414e4afeb8e1579a9bf587f85a6
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-09-10 23:12:38 -0300

    feat: ensure MultiStream receives correct options

--- src/infra/gateways/node-stream.ts
+++ src/infra/gateways/node-stream.ts
@@ -6,7 +6,7 @@ import { promisify } from 'util'
 export class NodeStream implements WriteStream {
   async writeOn ({ item, itemPath }: WriteStream.Input): WriteStream.Output {
     const pipelineAsync = promisify(pipeline)
-    const readStream = new MultiStream(item, {})
+    const readStream = MultiStream.readStream({ item })
     const writeStream = createWriteStream(itemPath)
     await pipelineAsync(
       readStream,
@@ -15,14 +15,31 @@ export class NodeStream implements WriteStream {
   }
 }
 
-class MultiStream extends Readable {
-  constructor (private object: any, options: ReadableOptions) {
-    super(object instanceof Buffer || typeof object === 'string' ? options : { objectMode: true })
+type ReadStreamInput = {
+  item: any
+  options?: object
+}
+
+export class MultiStream extends Readable {
+  multiStreamOptions?: object
+
+  private constructor (
+    private item: any,
+    options: ReadableOptions = {}
+  ) {
+    super(options)
+  }
+
+  static readStream ({ item, options }: ReadStreamInput): MultiStream {
+    const multiStreamOptions = item instanceof Buffer || typeof item === 'string' ? options : { objectMode: true }
+    const multiStream = new MultiStream(item, multiStreamOptions)
+    multiStream.multiStreamOptions = multiStreamOptions
+    return multiStream
   }
 
   override _read = (): void => {
-    this.push(this.object)
+    this.push(this.item)
     this.push(null)
-    this.object = null
+    this.item = null
   }
 }

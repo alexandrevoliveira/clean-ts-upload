051099bd8cb39d45ba07eab93d435a46b699d940
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-09-27 23:51:47 -0300

    chore: add Dockerfile.dev

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,5 +4,5 @@ export const env = {
   paths: {
     upload: process.env.LOCAL_UPLOAD_PATH ?? join(__dirname, '../../../tmp')
   },
-  port: process.env.PORT ?? 3000
+  port: process.env.PORT ?? '3000'
 }

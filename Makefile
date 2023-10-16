e9fdf9cd0b8c984d99f4caf5e90bd4da481a0b75
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-10-15 23:21:51 -0300

    chore: add container scripts

---
+++ Makefile
@@ -0,0 +1,4 @@
+build:
+	./scripts/build-dockerfile.sh
+run:
+	./scripts/run-dockerfile.sh
---
+++ scripts/build-dockerfile.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -o errexit
+
+readonly DIRECTORY="${DIRECTORY:-$(pwd)}"
+readonly IMAGE_FILE="${IMAGE_FILE:-Dockerfile.dev}"
+readonly IMAGE_PREFIX="${IMAGE_PREFIX:-alexandrevoliveira}"
+readonly IMAGE_NAME="${IMAGE_NAME:-clean-ts-upload}"
+readonly IMAGE_TAG="${IMAGE_TAG:-local}"
+
+
+main () {
+  local image_tag_ref=$IMAGE_PREFIX/$IMAGE_NAME:$IMAGE_TAG
+
+  echo -e "\nBuilding image: $image_tag_ref"
+
+  echo -e "\nBuild params:
+    --progress=plain \
+    --no-cache \
+    --tag "${image_tag_ref}" \
+    --file "${DIRECTORY}/${IMAGE_FILE}" \
+    "${DIRECTORY}"\n"
+
+  docker build \
+    --progress=plain \
+    --no-cache \
+    --tag "${image_tag_ref}" \
+    --file "${DIRECTORY}/${IMAGE_FILE}" \
+    "${DIRECTORY}"
+
+  echo -e "\nImage built: $image_tag_ref"
+}
+
+main
